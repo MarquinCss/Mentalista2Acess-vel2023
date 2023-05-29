7df7ecf499d0cb898b2f2565b3582929f84eb533
Marco Antônio Marchi Carreiro <antoniomarchi2006@gmail.com>
2023-05-29 20:58:34 -0300

    update
    a

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,42 +1,39 @@
-const elementoChute = document.getElementById('chute');
+const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.lang = 'pt-Br';
-recognition.start();
+recognition.lang = 'pt-Br'
+recognition.start()
 
-recognition.addEventListener('result', onSpeak);
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-  const chute = e.results[0][0].transcript;
-  exibeChuteNaTela(chute);
-  verificaSeOChutePossuiUmValorValido(chute);
-
-  if (chuteForInvalido(chute)) {
-    if (chute.toUpperCase() === 'GAME OVER') {
-      document.body.innerHTML = `
-        <h2>Acabou!</h2>
-        <h3>Pressione o botão para jogar novamente</h3>
-        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
-      `;
-      document.body.style.backgroundColor = 'black';
-    } else {
-      elementoChute.innerHTML += '<div>Valor Inválido</div>';
-    }
-  }
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChuteNaTela(chute) {
-  elementoChute.innerHTML = `
-    <div>Você disse</div>
-    <span class="box">${chute}</span>
-  `;
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
+if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GA") {
 
-function chuteForInvalido(chute) {
-  // Implemente a lógica da verificação de chute inválido aqui
-  // Retorne true se o chute for inválido, caso contrário, retorne false
-}
+            document.body.innerHTML =
+                `
+                <h2>Acabou!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
 
-recognition.addEventListener('end', () => recognition.start());
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
+recognition.addEventListener('end', () => recognition.start())
