19dd117d764ed7197c81eb25a73938a6dbcc4362
Marco Marchi <115740827+MarquinCss@users.noreply.github.com>
2023-05-29 00:02:22 -0300

    Update validacao.js
    adicionando animação

--- app/validacao.js
+++ app/validacao.js
@@ -22,11 +22,11 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        <div>O número secreto é menor <i class="fa-solid fa-down-long fa-bounce"></i></div>
         `
     } else {
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        <div>O número secreto é maior <i class="fa-solid fa-up-long fa-bounce"></i></div>
         `
     }
 }
